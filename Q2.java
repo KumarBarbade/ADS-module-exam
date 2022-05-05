26b3a3eaed8e367f630f94ba3f284c780f860445
Kumar Barbade <98301635+KumarBarbade@users.noreply.github.com>
2022-05-05 19:53:04 +0530

    Add files via upload

--- Q2.java
+++ Q2.java
@@ -1,89 +1,63 @@
 import java.util.*;
-class LinkedList{
-	Node head;
-	
-	static class Node{
-		int data;
-		Node next;
-		
-		Node(int d){
-			 data=d;
-			 next=null;
-		}
-	}
-	
-	void insert(int data){
-	
-		Node new_node= new Node(data);
-		if(head == null){
-			head = new_node;
-		}
-		
-		
-		if(head !=null){
-			Node n= head;
-	
-		while(n.next != null){
-			
-			n=n.next;
-			
-			}
-		n.next = new_node;
-		return;
-		
-	}
-	/*
-		void reverse(){
-			Node n=head;
-			
-		while(n.next!=null){
-			n=n.next;
-		}
-		*/
-			
-			
-		
-		
-		
-	void display(){
-	
-	Node n= head;
-	while(n != null){
-		System.out.print(n.data+" ");
-		n=n.next;
-		}
-	}
-	
 
-   public static void main(String[] args){
-	Scanner sc= new Scanner(System.in);
-	int n= sc.nextInt(); //1
-	int n1= sc.nextInt(); //5
 
-	Sorting s1= new Sorting(1);
-	
-	for(int i=2;i<=n1;i++){
-		a1[i]=sc.nextInt();
+class SLL{
+ Node head;
+ 
+ static class Node{
+	 int data;
+	 Node next;
+	 
+	Node(int d){
+		data=d;
+		next=null;
 	}
-	for(int i=2;i<=n1;i++){
-		s1.insert(a1[i]);
-	}
-	
-	s1.display();
-	
-	int n= sc.nextInt(); //1
-	int n1= sc.nextInt(); //5
+ }
+ 
+ 
+ 
+Node reverse(int data ){
+	Node nd = new Node(data);	 
+	 nd.next=head;
+	 head=nd;
+	return head;
+ }
+ 
 
-	Sorting s2= new Sorting(3);
+ void display(){
+	 Node n=head;
+	 while(n !=null){
+		 System.out.print(n.data+" ");
+		 n=n.next;
+	 }
+ }
+ 
+ 
+ public static void main(String[] args){
+	 Scanner sc= new Scanner(System.in);
 	
-	for(int i=2;i<=n1;i++){
-		a1[i]=sc.nextInt();
-	}
-	for(int i=2;i<=n1;i++){
-		s1.insert(a1[i]);
-	}
-	
-	s1.display();
-	
-	}
+	 int Testcase=sc.nextInt();
+	 int n=sc.nextInt();
+	 
+	 SLL L = new  SLL();	
+	 /*
+	 for(int i=0;i<n;i++){
+		int n1=sc.nextInt();
+		L.insert(n1);
+	 }
+	 L.display();
+	 */
+	 for(int i=0;i<n;i++){
+		int n1=sc.nextInt();
+		L.reverse(n1);
+		 
+	 }
+	L.display();
+		 
+	 
+	 
+ }
+
+ 
 }
+		
